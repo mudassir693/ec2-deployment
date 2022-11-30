17bab3628de9e51aae190d7dfcebeeba0b195df9
mudassir619 <mudassirsiddiqui27@gmail.com>
2022-11-30 16:48:08 +0500

    update repository

--- server.js
+++ server.js
@@ -5,13 +5,14 @@ const app = express()
 
 app.get('/',(req,res)=>{
     try {
-        return res.status(200).json({data:"everything works fine", error:false})
+        return res.status(200).json({data:"everything works fine and code update", error:false})
     } catch (error) {
         console.log(error)
         return res.status(500).json({data:"check console: error", error:true})
     }
 })
-const port = process.env.port ||5000 
+
+const port = process.env.port ||5000
 app.listen(port,()=>{
     console.log('server running on port ',port)
 })
