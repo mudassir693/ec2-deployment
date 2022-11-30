0dfefcbdd4034955f5f4314669a375612578a05f
mudassir619 <mudassirsiddiqui27@gmail.com>
2022-11-30 16:23:40 +0500

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from 'express'
+
+
+const app = express()
+
+app.get('/',(req,res)=>{
+    try {
+        return res.status(200).json({data:"everything works fine", error:false})
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({data:"check console: error", error:true})
+    }
+})
+const port = process.env.port ||5000 
+app.listen(port,()=>{
+    console.log('server running on port ',port)
+})
